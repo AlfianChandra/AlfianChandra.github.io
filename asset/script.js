1e61f9e0d4db82c3801994a8c5e2cdb3f43a6267
Alfian Chandra <31608738+AlfianChandra@users.noreply.github.com>
2017-10-07 06:09:07 +0700

    Add files via upload

---
+++ asset/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+  var sr = ScrollReveal();
+  sr.reveal(".rev",{
+    origin:"top",
+    duration:2000,
+    scale:0.5
+  });
+  
+  function switchPage(url)
+  {
+    $(".page").fadeOut(1000);
+    $.ajax({
+      url:url,
+      type:"get",
+      cache:false,
+      async:true,
+      success:function(res)
+      {
+        var t = 2;
+        var inter = setInterval(function(){
+          t--;
+          if(t === 0)
+          {
+            $(".page").fadeIn(1000);
+            $(".page").html(res);
+            
+            clearInterval(inter);
+          }
+        },500);
+      }
+    });
+  }
+  
+  $(window).load(function(){
+    switchPage("home.html");
+  });
+  
+  $(".page-switch").click(function(){
+    var page = $(this).attr("target-to");
+    switchPage(page);
+  });
+});
+
